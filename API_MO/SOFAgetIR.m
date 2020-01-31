a0780218b64a000e0a88553e06ac7afc84d278f2
julien.demuynke <julien.demuynke.org>
2020-01-31 11:03:37 +0100

    Added SOFAgetIR.m which outputs the IR data corresponding to a desired mic position and lpk position

---
+++ API_MO/SOFAgetIR.m
@@ -0,0 +1,37 @@
+function IR = SOFAgetIR(Obj,mic_pos_idx,lpk_pos_idx)
+
+if ~strcmp(Obj.GLOBAL_SOFAConventions,'AmbisonicsDRIR') || ~strcmp(Obj.GLOBAL_SOFAConventionsVersion,'2.0')
+    
+    error('Only AmbisonicsDRIR v2.0 convention is supported!');
+    
+end
+
+nof_mic_pos = length(nonzeros(any(Obj.Midx,2)));
+
+if nargin < 3
+    
+    if nargin < 2
+        
+        mic_pos_idx = input(sprintf('Enter the desired microphone position (1->%i): [1]\n',nof_mic_pos));
+        
+        if isempty(mic_pos_idx)            
+            mic_pos_idx = 1;            
+        elseif mic_pos_idx < 1 || mic_pos_idx > nof_mic_pos            
+            error('The desired microphone position must be an integer between 1 and %i!',nof_mic_pos);
+        end
+    
+        lpk_pos_idx = input(sprintf('Enter the desired loudspeaker position (1->%i): [all positions associated to microphone position %i] \n',length(nonzeros(Obj.Midx(mic_pos_idx,:))),mic_pos_idx));
+    
+        if isempty(lpk_pos_idx)
+             lpk_pos_idx = nonzeros(Obj.Midx(mic_pos_idx,:)); 
+        elseif lpk_pos_idx < 1 || lpk_pos_idx > length(nonzeros(Obj.Midx(mic_pos_idx,:)))            
+            error('The desired loudspeaker position must be an integer between 1 and %i!',length(nonzeros(Obj.Midx(mic_pos_idx,:))));
+        end
+        
+    end
+    
+end
+
+fprintf('Desired microphone position: %i\n\tDesired loudspeaker position: %s\n',mic_pos_idx,sprintf('%i ',lpk_pos_idx));
+
+IR = Obj.Data.IR(Obj.Midx(mic_pos_idx,lpk_pos_idx),:,:,:);
