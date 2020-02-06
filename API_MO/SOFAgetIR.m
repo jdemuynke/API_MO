c4a9bddc20223f84d12005564af5f634b764f641
julien.demuynke <julien.demuynke.org>
2020-02-06 19:04:39 +0100

    Added a function header + added the specific case of mic_pos_idx not being specified

--- API_MO/SOFAgetIR.m
+++ API_MO/SOFAgetIR.m
@@ -1,3 +1,24 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%   function IR = SOFAgetIR(Obj,mic_pos_idx,lpk_pos_idx)
+%
+%   This function extracts the IR data based on the indices of both the
+%   microphone position and loudspeaker position. 
+%
+%   mic_pos_idx and lpk_pos_idx are optional arguments. If not specified,
+%   the user will be prompted for choosing them. If so, mic_pos_idx can
+%   still be left empty: in that case, IR is an array containing all 
+%   valid IR data, sorted by increasing index of microphone positions (this
+%   is equivalent to removing the all-zeros data from Obj.Data.IR along the
+%   1rst dimension). If mic_pos_idx is specified, lpk_pos_idx can also be
+%   left empty: in that case, IR is an array containing all valid IR data
+%   for the specified microphone position.
+%
+%   The index of the loudspeaker position is global (global indexing 
+%   across the whole measurements dataset).
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function IR = SOFAgetIR(Obj,mic_pos_idx,lpk_pos_idx)
 
 if ~strcmp(Obj.GLOBAL_SOFAConventions,'AmbisonicsDRIR') || ~strcmp(Obj.GLOBAL_SOFAConventionsVersion,'2.0')
@@ -14,26 +35,27 @@ if nargin < 3
         
         mic_pos_idx = input(sprintf('Enter the desired microphone position (1->%i) [1]:\n',nof_mic_pos));
         
-        if isempty(mic_pos_idx)            
-            mic_pos_idx = 1;            
-        elseif mic_pos_idx < 1 || mic_pos_idx > nof_mic_pos            
+        if isempty(mic_pos_idx)
+            ListenerSourcePairIndex_tranposed = transpose(Obj.ListenerSourcePairIndex);
+            mic_pos_idx = ListenerSourcePairIndex_tranposed(find(transpose(~isnan(Obj.ListenerSourcePairIndex))));
+            IR = Obj.Data.IR(mic_pos_idx,:,:,:);
+            fprintf('Removing the all-zeros data from Obj.Data.IR along the 1rst dimension.\n');
+            return;
+        elseif mic_pos_idx < 1 || mic_pos_idx > nof_mic_pos
             error('The desired microphone position must be an integer between 1 and %i!',nof_mic_pos);
         end
-    
-        lpk_pos_idx = input(sprintf('Enter the desired loudspeaker position (1->%i) [all positions associated to microphone position %i]: \n',length(find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:)))),mic_pos_idx));
-    
-        if isempty(lpk_pos_idx)
-             lpk_pos_idx = Obj.ListenerSourcePairIndex(mic_pos_idx,find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:)))); 
-        elseif lpk_pos_idx < 1 || lpk_pos_idx > length(find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:))))
-            error('The desired loudspeaker position must be an integer between 1 and %i!',length(find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:)))));
-        end
         
-    end
+    end    
+    
+    lpk_pos_idx = input(sprintf('Enter the desired loudspeaker position from the list: %s [all positions]: \n',num2str(find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:))))));
+    
+    if isempty(lpk_pos_idx)
+        lpk_pos_idx = find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:)));
+    elseif ~find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:))==lpk_pos_idx)
+        error('The desired loudspeaker position must be an integer from the list %s!',num2str(find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:)))));
+    end 
     
 end
 
 fprintf('Desired microphone position: %i\n\tDesired loudspeaker position: %s\n',mic_pos_idx,sprintf('%i ',lpk_pos_idx));
-
-[~,ind] = find(~isnan(Obj.ListenerSourcePairIndex(mic_pos_idx,:)));
-
-IR = Obj.Data.IR(Obj.ListenerSourcePairIndex(mic_pos_idx,ind(lpk_pos_idx)),:,:,:);
+IR = Obj.Data.IR(Obj.ListenerSourcePairIndex(mic_pos_idx,lpk_pos_idx),:,:,:);
